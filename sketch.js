b2b1c8fd87b658190bff7265e3bcb82950f0fa30
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-06-01 17:12:38 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,75 +17,51 @@ var food;
 var rabbit;
 
 var button;
-var bunny;
+var bunny; // adicionar <<<<<<<<<<<<<<<<<<
 
-function preload()
-{
+function preload()  {
   bg_img = loadImage('background.png');
   food = loadImage('melon.png');
   rabbit = loadImage('Rabbit-01.png');
 }
-
-function setup() 
-{
+function setup() {
   createCanvas(500,700);
   frameRate(80);
-
   engine = Engine.create();
   world = engine.world;
-
   //btn 1
-  button = createImg('cut_btn.png');
-  button.position(200,30);
-  button.size(50,50);
-  button.mouseClicked(drop);
-
-  
+  button = createImg('cut_btn.png'); // adicionar <<<<<<<<<<<<<<<<<<
+  button.position(200,30);// adicionar <<<<<<<<<<<<<<<<<<
+  button.size(50,50);// adicionar <<<<<<<<<<<<<<<<<<
+  button.mouseClicked(drop); // adicionar <<<<<<<<<<<<<<<<<<
   rope = new Rope(8,{x:220,y:30});
   ground = new Ground(200,690,600,20);
-  bunny = createSprite(200,620,100,100);
-  bunny.addImage(rabbit);
-  bunny.scale = 0.2;
-
+  bunny = createSprite(200,620,100,100);// adicionar <<<<<<<<<<<<<<<<<<
+  bunny.addImage(rabbit);// adicionar <<<<<<<<<<<<<<<<<<
+  bunny.scale = 0.2;// adicionar <<<<<<<<<<<<<<<<<<
   fruit = Bodies.circle(300,300,20);
   Matter.Composite.add(rope.body,fruit);
-
   fruit_con = new Link(rope,fruit);
-
   rectMode(CENTER);
   ellipseMode(RADIUS);
   textSize(50)
-  
 }
-
-function draw() 
-{
+function draw() {
   background(51);
-  image(bg_img,0,0,displayWidth+80,displayHeight);
-
+  image(bg_img,width/2,height/2,490,690);
   push();
   imageMode(CENTER);
-  if(fruit!=null){
+  if(fruit!=null){// adicionar <<<<<<<<<<<<<<<<<<
     image(food,fruit.position.x,fruit.position.y,70,70);
-  }
+  }// adicionar <<<<<<<<<<<<<<<<<<
   pop();
-
   rope.show();
-
   Engine.update(engine);
   ground.show();
   drawSprites();
-   
 }
-
-function drop()
-{
-  rope.break();
-  fruit_con.detach();
-  fruit_con = null; 
+function drop() { // adicionar <<<<<<<<<<<<<<<<<<
+  rope.break(); // adicionar <<<<<<<<<<<<<<<<<<
+  fruit_con.detach(); // adicionar <<<<<<<<<<<<<<<<<<
+  fruit_con = null; // adicionar <<<<<<<<<<<<<<<<<<
 }
-
-
-
-
-
